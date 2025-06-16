380800bda0863f8810e7382646bca8f121318763
Mwesigwa Gilbert <89534467+Elekkein@users.noreply.github.com>
2025-06-16 14:05:22 +0300

    Add files via upload

--- src/UTMS/Lecturer.java
+++ src/UTMS/Lecturer.java
@@ -1,19 +1,21 @@
-package UT;
+package UTMS;
 
-public class Lecturer extends User{
+//Lecturer subclass
+public class Lecturer extends User {
+ private String employeeId;
+ private String department;
 
-	private String employeeId;
-	private String department;
-	
-	public Lecturer(String userId, String name, String email, String password, String employeeId, String department) {
-		super(userId, name, email, password);
-		this.department=department;
-		this.employeeId=employeeId;
-	}
-	
-	@Override
-	public void requestTransport(String destination) {
-		System.out.println("Lecturer "+ getName() + " requesting transport to "+ destination + " for teaching purposes");
-	}
-	
+ public Lecturer(String userId, String name, String email, String password,
+                String employeeId, String department) {
+     super(userId, name, email, password);
+     this.employeeId = employeeId;
+     this.department = department;
+ }
+
+ @Override
+ public void requestTransport(String destination) {
+     System.out.println("Lecturer " + getName() + " requesting transport to " + destination + 
+                       " for teaching/research purposes");
+     // Additional lecturer-specific logic
+ }
 }
